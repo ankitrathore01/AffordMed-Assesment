a2f50058cec0121e73b5d61dcdf97e62663fc7ae
Ankit Rathore <rathoreankit01091999@gmail.com>
2021-09-27 22:27:34 +0530

    initial files uploaded

---
+++ sol1.py
@@ -0,0 +1,11 @@
+a int (input())
+b = list(map(str, input().strip(),split(' ')))
+class TCP:
+    def c(a,b):
+        ans =0
+        if(len(data)>0):
+            ans=s+len(b)+a
+        else:
+            ans=a+ans
+        print(ans)
+    c(a,b)
